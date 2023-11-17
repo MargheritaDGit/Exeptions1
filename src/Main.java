707e468f2affdd5a301790f01d555d8d5b88a2b2
Margherita <margherita.deodato14@gmail.com>
2023-11-17 12:36:48 +0100

    commit exeptions1 nuovo con correzione

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,7 @@
 //Scrivere una funzione che controlli se un numero int è in un determinato range
 //Se lo è ritorna true se non lo è lancia un'eccezione.
 
-//ESEGUITO DA MARGHERITA DEODATO
 public class Main {
-
-    private static boolean ExeptionRange(int numeroCheck, int rangeMin, int rangeMax) {
-        return numeroCheck >= rangeMin && numeroCheck <= rangeMax;}
-
-        public static void main(String[] args) {
-
-            int numeroCheck = 100;
-            int rangeMin = 10;
-            int rangeMax = 200;
-
-
-        if (ExeptionRange(numeroCheck, rangeMin, rangeMax)) {
-            System.out.println(numeroCheck + " è nel range compreso tra " + rangeMin + " e " + rangeMax);
-        } else {
-            System.out.println(numeroCheck + " non è nel range compreso tra " + rangeMin + " e " + rangeMax);
-        }
-    }
-}
-
-// ESEGUITO DA SIMONE LIOTTA
-
-/*public class EsercizioExceptions1 {
     public static void main(String[] args) {
 
         try {
@@ -42,6 +19,5 @@ public class Main {
         } else {
             throw new RuntimeException("Il numero non è nel range");
         }
-
     }
-}*/
+}
